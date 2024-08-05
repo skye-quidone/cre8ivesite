43128f2b2721ee89ba91a8e4f0a0a98c0f8d3250
skye-quidone <skye@cre8ivenyc.com>
2024-08-05 13:00:55 -0400

    Update front_page.js

--- pages/front_page.js
+++ pages/front_page.js
@@ -1,8 +1,28 @@
-// mouse over on cards //
-
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.0/jquery.min.js"></script>
-<script>
-$(document).ready(function(){
+<!-- home page- mouseover for process -->
+< script src = "https://ajax.googleapis.com/ajax/libs/jquery/3.7.0/jquery.min.js" > < /script> 
+< script >
+  $(document).ready(function() {
     $('.block a:contains("clickable")').addClass('text-click');
-});
+  }); 
+</script>
+
+
+<!--fade swap words for home header -->
+<script>
+var text = ["STUDIOS.", "LOFTS.", "OFFICES."];
+var counter = 0;
+var elem = document.getElementById("changeText");
+setInterval(change, 7000);
+
+function change() {
+  elem.classList.add('hide');
+  setTimeout(function() {
+    elem.innerHTML = text[counter];
+    elem.classList.remove('hide');
+    counter++;
+    if (counter >= text.length) {
+      counter = 0;
+    }
+  }, 200);
+}
 </script>
