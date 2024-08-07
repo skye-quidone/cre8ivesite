6d1abe6cb0e4bf4361db4eb0a2d6d99cdf42a774
skye-quidone <skye@cre8ivenyc.com>
2024-08-07 15:58:58 -0400

    Update js_sheet.js

--- js_sheet.js
+++ js_sheet.js
@@ -1,3 +1,5 @@
+<script src="https://kit.fontawesome.com/dd04f97c73.js" crossorigin="anonymous"></script>
+    
 <!-- home page- mouseover for process -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.0/jquery.min.js"></script>
 <script>
